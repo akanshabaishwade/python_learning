2cf22c4195e91c892578d2bb9b637a9c3b1b30db
akanshabaishwade <akanshabaishwade@gmail.com>
2021-06-09 21:31:44 +0530

    tic tac toe game

--- practice/fib.py
+++ practice/fib.py
@@ -1,21 +1,35 @@
-turm = 100
-first, Secound = 35, 36
-count = 0
-
-
 print("Fibonacci sequence:")
-print(first)
-print(Secound)
-while count < turm:
+def add(first, Secound):
+    turm = 100
+    count = 0
+    print(first)
+    print(Secound)
 
-    nth = first + Secound
+    while count < turm:
 
-    first = Secound
-    Secound = nth
-    count += 1
-    if nth >= 1000:
-        break
-    # i = d.append(nterms)
-    print(nth)
 
+        nth = first + Secound
+
+        first = Secound
+
+        Secound = nth
+        count += 1
+        if nth >= 1000:
+            break
+
+        print(nth)
+add1 = add(35, 36)
+add2 = add(70, 80)
+main_siries = []
+
+for i in range(len(add1)):
+    main_siries.append( add1[i] + add2[i])
+print(main_siries)
+
+
+# main_siries = [add1[x] + add2[x] for x in range(len(add1))]
+# print(main_siries)
+
+# main_siries = list(map(add, add1, add2))
+# print(main_siries)
 
