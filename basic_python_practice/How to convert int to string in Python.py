59ce5c9b28c9060d47e7bd8f9fe235e8464c4905
akanshabaishwade <akanshabaishwade@gmail.com>
2021-06-12 16:10:43 +0530

    basic pyhton

---
+++ basic_python_practice/How to convert int to string in Python.py
@@ -0,0 +1,7 @@
+number = 100 # this variable value is 'int'
+print(number)
+print(type(number))  # print type of number
+# use str for change int to str
+new_str_num = str(number)
+print(new_str_num)
+print(type(new_str_num))
---
+++ basic_python_practice/how to convert int to float.py
@@ -0,0 +1,11 @@
+variable = 10
+
+print("x is of type:", type(variable))
+
+float = 10.6
+print("y is of type:", type(float))
+
+x = variable + float
+
+print(x)
+print("x is of type:", type(x))
