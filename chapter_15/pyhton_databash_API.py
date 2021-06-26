798aa08d4f77300f0ed8e6c044283bff71696e32
akanshabaishwade <akanshabaishwade@gmail.com>
2021-06-26 15:11:28 +0530

    project and exercise in pyhton

--- chapter_15/pyhton_databash_API.py
+++ chapter_15/pyhton_databash_API.py
@@ -4,3 +4,17 @@
 #-----
 # | |
 
+for row in range(5):
+    if row % 2 ==0 :
+        current = (row/2)
+        for column in range(5):
+            if column% 2 == 0:
+                current = (column/2)
+                if column != 4:
+                    print([row][column], end="")
+                else:
+                    print([row][column])
+            else:
+                print("|", end="")
+    else:
+        print("-----")
