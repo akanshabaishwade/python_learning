6d2f65d495e8f8bc0ee40e7ab45d2ab156726969
akansha baishwade <akanshabaishwade@gmial.com>
2021-08-20 12:43:23 +0530

    pygame game project

---
+++ RIRM_Round-1_Interview_Questions/Questions_1.py
@@ -0,0 +1,15 @@
+' a. To check if the year is a Leap year or not '
+# Python program to check if year is a leap year or not
+
+year = input(int())
+
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
