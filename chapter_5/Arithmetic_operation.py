482b3c76b60e5d6c24d9ea2dce7039abe5ab9d0f
akansha baishwade <akanshabaishwade@gmial.com>
2021-08-29 16:53:40 +0530

    Decorater

--- chapter_5/Arithmetic_operation.py
+++ chapter_5/Arithmetic_operation.py
@@ -1,4 +1,11 @@
-x = 4
-y = 3
-z = x +y
-print(f'result is {z}')
+SET max as 60
+SET num as 1
+SET sum as 0
+FUNCTION Ops ()
+	WHILE num is less than max
+		IF num is completely divisible by 2 AND 3
+			SET sum to (sum + num)
+		ENDIF
+		SET num to num+1
+	ENDWHILE
+	PRINT "The result is", sum
